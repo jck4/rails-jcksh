ca64c12e6f3d7b313afb07ffa2836912ae03c8a9
jck <jck>
2025-02-12 16:17:17 -0500

    Added and removed many things

--- jck.sh/config/routes.rb
+++ jck.sh/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "pages/about"
   get "home/index"
+
   get "projects", to: "projects#index", as: "projects"
   get 'about', to: 'pages#about', as: 'about'
+  get 'blog', to: 'pages#blog', as: 'blog'
   get 'aquarium', to: 'pages#aquarium', as: 'aquarium'
   root 'home#index'
   get "up" => "rails/health#show", as: :rails_health_check
